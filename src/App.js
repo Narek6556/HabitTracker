d63de11a27a978838bc2c9d50fb170d8c6b1a47e
Narek Aghasyan <aghasyan.na@gmail.com>
2021-11-29 00:01:40 +0400

    simple check - everything is OK

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {SafeAreaView, Text, View} from "react-native";
 
 /**
  * Navigation Stacks
@@ -9,10 +10,25 @@ import {createNativeStackNavigator} from "@react-navigation/native-stack";
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const stylesForText = {
+    color: 'white',
+    fontSize: 23,
+    justifyContent: 'center',
+    backgroundColor: 'red',
+  };
+  const stylesForView = {
+    alignItems: 'center',
+    height: 200,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: 'blue',
+  };
   return (
-    <NavigationContainer>
-
-    </NavigationContainer>
+    <SafeAreaView>
+      <View style={stylesForView}>
+        <Text style={stylesForText}>Hellooo</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
