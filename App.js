b1e14dde8d8b015641c03498db7ad20a3a0e3937
Narek Aghasyan <aghasyan.na@gmail.com>
2021-11-28 23:36:40 +0400

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+
+/**
+ * Navigation Stacks
+ * */
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+
+    </NavigationContainer>
+  );
+};
+
+export default App;
